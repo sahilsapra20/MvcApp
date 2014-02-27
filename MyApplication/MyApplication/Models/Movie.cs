ae2df67d11cb1425ea4972c599e4dd24b066458e
sahil <sahilsapra@ymail.com>
2014-02-26 22:30:10 -0500

    added the first file

---
+++ MyApplication/MyApplication/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApplication.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateTime releasedate { get; set; }
+        public string genre { get; set; }
+        public decimal price { get; set; }
+    }
+}
